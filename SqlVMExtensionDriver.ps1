f2961e0ea7456a33b6437bf2459ac1391a3f21fd
chrisbaudo <chrisbaudo@hotmail.com>
2021-04-01 09:58:00 -0500

    Initial commit

---
+++ SqlVMExtensionDriver.ps1
@@ -0,0 +1,31 @@
+param (
+    [string]$storageAccountName,
+    [string]$storageContainerName,
+    [string]$containerSAS,
+    [string]$databaseName,
+    [string]$databaseBackupName,
+    [string]$sqlUserName,
+    [string]$sqlPassword,
+    [string]$cosmosDBConnectionString,
+    [string]$cosmosDBDatabaseName
+)
+
+.\DeploySQLVM.ps1 -storageAccountName $storageAccountName `
+    -storageContainerName $storageContainerName `
+    -containerSAS $containerSAS `
+    -databaseName $databaseName `
+    -databaseBackupName $databaseBackupName `
+    -sqlUserName $sqlUserName `
+    -sqlPassword $sqlPassword
+
+.\DeployCosmosDB.ps1 -storageAccountName $storageAccountName `
+    -storageContainerName $storageContainerName `
+    -containerSAS $containerSAS `
+    -databaseName $databaseName `
+    -databaseBackupName $databaseBackupName `
+    -sqlUserName $sqlUserName `
+    -sqlPassword $sqlPassword `
+    -cosmosDBConnectionString $cosmosDBConnectionString `
+    -cosmosDBDatabaseName $cosmosDBDatabaseName
+
+.\DisableIEESC.ps1
