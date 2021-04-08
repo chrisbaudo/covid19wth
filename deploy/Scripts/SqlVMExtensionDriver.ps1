446dab0014467c3113d0d281927e461f65258b05
chrisbaudo <chrisbaudo@hotmail.com>
2021-04-08 07:01:38 -0500

    Add files via upload

---
+++ deploy/Scripts/SqlVMExtensionDriver.ps1
@@ -0,0 +1,24 @@
+param (
+    [string]$ohmdwSqlserverName,    
+    [string]$databaseName,
+    [string]$databaseBackupName,
+    [string]$sqlUserName,
+    [string]$sqlPassword,
+    [string]$cosmosDBConnectionString,
+    [string]$cosmosDBDatabaseName
+)
+
+.\DeploySQLVM.ps1 -ohmdwSqlserverName $ohmdwSqlserverName `
+    -databaseName $databaseName `
+    -databaseBackupName $databaseBackupName `
+    -sqlUserName $sqlUserName `
+    -sqlPassword $sqlPassword
+
+.\DeployCosmosDB.ps1 -databaseName $databaseName `
+    -databaseBackupName $databaseBackupName `
+    -sqlUserName $sqlUserName `
+    -sqlPassword $sqlPassword `
+    -cosmosDBConnectionString $cosmosDBConnectionString `
+    -cosmosDBDatabaseName $cosmosDBDatabaseName
+
+.\DisableIEESC.ps1
